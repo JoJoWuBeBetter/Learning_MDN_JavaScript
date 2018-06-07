7b7c89e8b6a8c2000dac3ef323e048223ccca60a
JokerWu <453840856@qq.com>
2018-06-07 11:42:22 +0800

    使用网页事件的方式

--- Events/main.js
+++ Events/main.js
@@ -4,7 +4,9 @@ function random(number) {
     return Math.floor(Math.random() * (number + 1));
 }
 
-btn.onclick = function () {
+function bgChange() {
     var rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
     document.body.style.backgroundColor = rndCol;
 }
+
+btn.onmouseover = bgChange
