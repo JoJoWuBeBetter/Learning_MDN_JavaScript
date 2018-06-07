a30cf14c121b89b7a4ecb95292131e4869437a93
JokerWu <453840856@qq.com>
2018-06-07 14:22:39 +0800

    Finish HomeWork

--- HomeWork/main.js
+++ HomeWork/main.js
@@ -6,9 +6,34 @@ var overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
+for (let index = 0; index < 5; index++) {
+  var imgName = 'images/pic' + (index + 1) + '.jpg'
   var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('src', imgName);
   thumbBar.appendChild(newImage);
+}
+
+function imgChage(e) {
+  var imgName = e.target.src;
+  displayedImage.src = imgName;
+}
+
+thumbBar.addEventListener('click', imgChage)
 
 /* Wiring up the Darken/Lighten button */
 
+function opacityChange(e) {
+  if (e.target.className === "dark") {
+    overlay.style.backgroundColor = 'black';
+    overlay.style.opacity = 0.5;
+    e.target.className = 'Light';
+    e.target.textContent = 'Lighten';
+  } else {
+    overlay.style.backgroundColor = '';
+    overlay.style.opacity = 0.5;
+    e.target.className = 'dark';
+    e.target.textContent = 'Darken';
+  }
+}
+
+btn.addEventListener('click', opacityChange);
