64fb8eb3ad8f53f48ef20102c2550f3684a67b76
JokerWu <453840856@qq.com>
2018-06-10 15:17:06 +0800

    让球动起来

--- Objects/Object_building_practice/main.js
+++ Objects/Object_building_practice/main.js
@@ -21,3 +21,62 @@ function Ball(x, y, velX, velY, color, size) {
     this.color = color;
     this.size = size;
 }
+
+Ball.prototype.draw = function () {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+}
+
+Ball.prototype.update = function () {
+    if ((this.x + this.size) >= width) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.x - this.size) <= 0) {
+        this.velX = -(this.velX);
+    }
+
+    if ((this.y + this.size) >= height) {
+        this.velY = -(this.velY);
+    }
+
+    if ((this.y - this.size) <= 0) {
+        this.velY = -(this.velY);
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+}
+var balls = [];
+
+function loop() {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+    ctx.fillRect(0, 0, width, height);
+
+    while (balls.length < 50) {
+        var xv = random(-7, 7)
+        var xy = random(-7, 7)
+        while (xy === 0 | xy === 0) {
+            xv = random(-7, 7)
+            xy = random(-7, 7)
+        }
+        var ball = new Ball(
+            random(20, width-20),
+            random(20, height-20),
+            xv,
+            xy,
+            'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
+            random(10, 20)
+        );
+        balls.push(ball);
+    }
+
+    for (var i = 0; i < balls.length; i++) {
+        balls[i].draw();
+        balls[i].update();
+    }
+
+    requestAnimationFrame(loop);
+}
